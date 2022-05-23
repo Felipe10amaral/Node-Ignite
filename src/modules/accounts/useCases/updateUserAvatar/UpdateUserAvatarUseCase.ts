32473ae4d428435e3ba04871856e9b799849a1fb
Felipe Amaral <felipeamaraldev15@gmail.com>
2022-05-23 00:53:25 -0300

    isolando o avatar das rotas

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,28 @@
+import { injectable, inject } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 
-class UpdateUserAvatarUseCase {}
+interface IRequest {
+    user_id: string;
+    avatar_file: string;
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ){}
+
+    async execute({avatar_file, user_id}: IRequest): Promise<void>{
+        const user = await this.usersRepository.findById(user_id);
+
+        user.avatar = avatar_file;
+
+        await this.usersRepository.create(user);
+    }
+}
 
 
 export {UpdateUserAvatarUseCase}
